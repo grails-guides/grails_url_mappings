2fe78c08901994529b7683c059dda511814263d9
Zachary Klein <zak@silver-chalice.com>
2018-01-17 15:23:55 -0600

    initial commit

---
+++ complete/grails-app/init/grails_101_day_two_url_mappings/Application.groovy
@@ -0,0 +1,10 @@
+package grails_101_day_two_url_mappings
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ complete/grails-app/init/grails_101_day_two_url_mappings/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails_101_day_two_url_mappings
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}

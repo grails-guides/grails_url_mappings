0f80f984215d735fa69e9364339fa547029059ff
Zachary Klein <zak@silver-chalice.com>
2018-01-21 23:37:33 -0600

    URL mapping guide

--- complete/grails-app/controllers/demo/UrlMappings.groovy
+++ complete/grails-app/controllers/demo/UrlMappings.groovy
@@ -9,6 +9,12 @@ class UrlMappings {
             }
         }
 
+
+        get "/talks"(controller: 'conference', action: 'talks') //<1>
+        get "/speakers"(controller: 'conference', action: 'speakers')
+        get "/agenda"(controller: 'conference', action: 'agenda')
+
+
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
